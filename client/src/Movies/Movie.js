be7586cd8a028ff3b7e739b08f13d5d141e345d6
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-02-16 23:42:24 -0700

    Made the form controlled.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -16,6 +17,10 @@ function Movie({ addToSavedList }) {
 
   const saveMovie = () => {
     addToSavedList(movie);
+  };
+
+  const editMovie = () => {
+    push(`/update-movie/${params.id}`);
   };
 
   useEffect(() => {
@@ -33,6 +38,10 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+
+      <button onClick={editMovie}>
+        Edit
+      </button>
     </div>
   );
 }
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -3,6 +3,34 @@ import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 function UpdateMovie() {
+    const { id } = useParams();
+
+    const [info, setInfo] = useState({
+        id: null,
+        title: '',
+        director: '',
+        metascore: '',
+        stars: []
+    });
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/movies/${id}`)
+        .then(res => {
+            console.log(res);
+            setInfo({
+                id: res.data.id,
+                title: res.data.title,
+                director: res.data.director,
+                metascore: res.data.metascore,
+                stars: res.data.stars
+            });
+        })
+        .catch(err => console.log(err));
+    }, []);
+
+    const handleChange = e => {
+        setInfo({...info, [e.target.name]: e.target.value});
+    };
 
     return (
         <div>
@@ -11,18 +39,24 @@ function UpdateMovie() {
                     <input
                     id='title'
                     name='title'
+                    value={info.title}
+                    onChange={handleChange}
                     />
                 </label>
                 <label htmlFor='director'>Director: 
                     <input
                     id='director'
                     name='director'
+                    value={info.director}
+                    onChange={handleChange}
                     />
                 </label>
                 <label htmlFor='metascore'>Metascore: 
                     <input
                     id='metascore'
                     name='metascore'
+                    value={info.metascore}
+                    onChange={handleChange}
                     />
                 </label>
                 <button>Update</button>
